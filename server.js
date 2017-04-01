ea0f47ee975a0cd34ccd89c03173dd48eade15bd
Nin7aStar <xiaoguangyu361@gmail.com>
2017-04-02 01:08:15 +0800

    Server and client side authentication

--- server.js
+++ server.js
@@ -251,7 +251,7 @@ app.post('/api/shows', function(req, res, next) {
 app.post('/api/subscribe', ensureAuthenticated, function(req, res, next) {
     Show.findById(req.body.showId, function(err, show) {
         if (err) return next(err);
-        show.subscribers.push(req.user._id);
+        show.subscribers.push(req.user.id);
         show.save(function(err) {
             if (err) return next(err);
             res.send(200);
@@ -262,7 +262,7 @@ app.post('/api/subscribe', ensureAuthenticated, function(req, res, next) {
 app.post('/api/unsubscribe', ensureAuthenticated, function(req, res, next) {
     Show.findById(req.body.showId, function(err, show) {
         if (err) return next(err);
-        var index = show.subscribers.indexOf(req.user._id);
+        var index = show.subscribers.indexOf(req.user.id);
         show.subscribers.splice(index, 1);
         show.save(function(err) {
             if (err) return next(err);

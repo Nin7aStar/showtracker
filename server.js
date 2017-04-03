2e986e2811bbd17453cf92d4b7358c721f9d35de
Nin7aStar <xiaoguangyu361@gmail.com>
2017-04-03 23:39:35 +0800

    Email Notifications

--- server.js
+++ server.js
@@ -12,9 +12,6 @@ var _ = require('lodash');
 var session = require('express-session');       // passport js
 var passport = require('passport');
 var LocalStrategy = require('passport-local').Strategy;
-var agenda = require('agenda')({ db: { address: 'localhost:27017/showtracker' } });
-var sugar = require('sugar');
-var nodemailer = require('nodemailer');
 
 // show schema
 var showSchema = new mongoose.Schema({
@@ -238,11 +235,6 @@ app.post('/api/shows', function(req, res, next) {
                 }
                 return next(err);
             }
-
-            // @707
-            // var alertDate = Date.create('Next ' + show.airsDayOfWeek + ' at ' + show.airsTime).rewind({ hour: 2});
-            // agenda.schedule(alertDate, 'send email alert', show.name).repeatEvery('1 week');
-
             res.send(200);
         });
     });
@@ -269,47 +261,6 @@ app.post('/api/unsubscribe', ensureAuthenticated, function(req, res, next) {
             res.send(200);
         });
     });
-});
-
-agenda.define('send email alert', function(job, done) {
-    Show.findOne({ name: job.attrs.data }).populate('subscribers').exec(function(err, show) {
-        var emails = show.subscribers.map(function(user) {
-            return user.email;
-        });
-
-        var upcomingEpisode = show.episodes.filter(function(episode) {
-            return new Date(episode.firstAired) > new Date();
-        })[0];
-
-        var smtpTransport = nodemailer.createTransport('SMTP', {
-            service: 'SendGrid',
-            auth: { user: 'hslogin', pass: 'hspassword00' }
-        });
-
-        var mailOptions = {
-            from: 'Fred Foo ✔ <foo@blurdybloop.com>',
-            to: emails.join(','),
-            subject: show.name + ' is starting soon!',
-            text: show.name + ' starts in less than 2 hours on ' + show.network + '.\n\n' +
-            'Episode ' + upcomingEpisode.episodeNumber + ' Overview\n\n' + upcomingEpisode.overview
-        };
-
-        smtpTransport.sendMail(mailOptions, function(error, response) {
-            console.log('Message sent: ' + response.message);
-            smtpTransport.close();
-            done();
-        });
-    });
-});
-
-agenda.start();
-
-agenda.on('start', function(job) {
-    console.log("Job %s starting", job.attrs.name);
-});
-
-agenda.on('complete', function(job) {
-    console.log("Job %s finished", job.attrs.name);
 });
 
 passport.serializeUser(function(user, done) {

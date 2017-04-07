e43d9317ba45554d735ebf228c9301417060ceef
Nin7aStar <xiaoguangyu361@gmail.com>
2017-04-07 22:07:19 +0800

    Add the Email Notification(unfinished)

--- server.js
+++ server.js
@@ -14,6 +14,10 @@ var passport = require('passport');
 var LocalStrategy = require('passport-local').Strategy;
 var compress = require('compression');
 
+var agenda = require('agenda')({ db: { address: 'localhost:27017/test' } });
+var sugar = require('sugar');
+var nodemailer = require('nodemailer');
+
 // show schema
 var showSchema = new mongoose.Schema({
     _id:            Number,
@@ -240,11 +244,15 @@ app.post('/api/shows', function(req, res, next) {
         if (err) return next(err);
         show.save(function(err) {
             if (err) {
-                if (err.code == 11000) {
-                    return res.send(409, { message: show.name + ' already exists.' });
-                }
+                // if (err.code == 11000) {
+                //     return res.send(409, { message: show.name + ' already exists.' });
+                // }
                 return next(err);
             }
+
+            var alertDate = Date.create('Next ' + show.airsDayOfWeek + ' at ' + show.airsTime).rewind({ hour: 2});
+            agenda.schedule(alertDate, 'send email alert', show.name).repeatEvery('1 week');
+
             res.send(200);
         });
     });
@@ -271,6 +279,47 @@ app.post('/api/unsubscribe', ensureAuthenticated, function(req, res, next) {
             res.send(200);
         });
     });
+});
+
+agenda.define('send email alert', function(job, done) {
+    Show.findOne({ name: job.attrs.data }).populate('subscribers').exec(function(err, show) {
+        var emails = show.subscribers.map(function(user) {
+            return user.email;
+        });
+
+        var upcomingEpisode = show.episodes.filter(function(episode) {
+            return new Date(episode.firstAired) > new Date();
+        })[0];
+
+        var smtpTransport = nodemailer.createTransport('SMTP', {
+            service: 'SendGrid',
+            auth: { user: 'hslogin', pass: 'hspassword00' }
+        });
+
+        var mailOptions = {
+            from: 'Fred Foo ✔ <foo@blurdybloop.com>',
+            to: emails.join(','),
+            subject: show.name + ' is starting soon!',
+            text: show.name + ' starts in less than 2 hours on ' + show.network + '.\n\n' +
+            'Episode ' + upcomingEpisode.episodeNumber + ' Overview\n\n' + upcomingEpisode.overview
+        };
+
+        smtpTransport.sendMail(mailOptions, function(error, response) {
+            console.log('Message sent: ' + response.message);
+            smtpTransport.close();
+            done();
+        });
+    });
+});
+
+agenda.start();
+
+agenda.on('start', function(job) {
+    console.log("Job %s starting", job.attrs.name);
+});
+
+agenda.on('complete', function(job) {
+    console.log("Job %s finished", job.attrs.name);
 });
 
 passport.serializeUser(function(user, done) {
